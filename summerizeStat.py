8d97cc085117affdced594c0e1bf78ff0383e12a
Hansika <hansikam94@gmail.com>
2021-04-07 14:53:41 -0400

    filter outing single stat to dictionary done

---
+++ summerizeStat.py
@@ -0,0 +1,44 @@
+import sys
+import csv
+import pandas as pd
+
+def read_single_stat_file( file, config_name ):
+    file1 = open(file, 'r')
+    lines = file1.readlines()
+    stat_dict = {'configuration': config_name}
+    for line in lines:
+        line = line.strip()
+        if len(line) < 1:
+            continue
+        line_content = line.split()
+        stat_dict[line_content[0]] = line_content[1]
+    file1.close()
+    return stat_dict
+
+def read_file_line_by_line():
+    file1 = open('targetStats', 'r')
+    lines = file1.readlines()
+    item_list = []
+    for line in lines:
+        item_list.append(line.strip())
+    file1.close()
+    return item_list
+
+def write_to_csv(file, data_dict):
+    with open(file, 'w') as output:
+        writer = csv.writer(output)
+        for key, value in data_dict.items():
+            writer.writerow([key, value])
+
+def filter_out_stats(original_dict):
+    target_stats = read_file_line_by_line()
+    target_stats.append('configuration')
+    filtered_dict = {k: v for (k, v) in original_dict.items() if k in target_stats}
+    return filtered_dict
+
+input_folder = sys.argv[1]
+output_file = sys.argv[2]
+
+out = read_single_stat_file('./m5out/FFT/config1/stats.txt', 'config1')
+out_new = filter_out_stats(out)
+write_to_csv("out.csv", out)
